acd241a7d9fb2ff2452b8fc5abc6c299f153fc95
junwoo <33277588+mike6321@users.noreply.github.com>
2021-03-25 00:06:33 +0900

    Dto Controller Test

--- springboot-webservice/src/main/java/me/choi/book/springboot/web/HelloController.java
+++ springboot-webservice/src/main/java/me/choi/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package me.choi.book.springboot.web;
 
+import me.choi.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -16,5 +18,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+
+        return new HelloResponseDto(name, amount);
     }
 }
